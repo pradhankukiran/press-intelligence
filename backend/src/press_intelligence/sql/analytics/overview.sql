8e23ddbd14f788cee7e5e3c59b3f294b12ee4b77
kiran <kiran@ecorrouge.com>
2026-04-19 18:51:39 +0530

    chore(backend): remove unused overview.sql (not referenced by any service)

--- backend/src/press_intelligence/sql/analytics/overview.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  DATE(published_at) AS publication_date,
-  COUNT(*) AS article_count
-FROM `{google_cloud_project}.{bigquery_dataset_raw}.articles_raw`
-WHERE DATE(published_at) BETWEEN DATE('{from_date}') AND DATE('{to_date}')
-GROUP BY publication_date
-ORDER BY publication_date;
